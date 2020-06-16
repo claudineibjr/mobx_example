f7a5c1ce75a087f6c1bedb3bc7ad7fe0e84f742d
Claudinei B Jr <claudineibjr@hotmail.com>
2020-06-16 13:29:47 -0300

    Reorganization of folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_bible/mobx/main_screen.dart';
+import 'package:quiz_bible/mobx/main_screen/main_screen.dart';
 // import 'package:quiz_bible/mobx/main_screen.dart';
 
 void main() {
